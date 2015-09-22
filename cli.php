f7ece38a793a09457e98d7a14c071fc13f123b6f
ms <schmiddim@gmx.at>
2015-09-23 00:03:56 +0200

    first commit

---
+++ cli.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: ms
+ * Date: 23.09.15
+ * Time: 00:02
+ *
+Github
+
+https://github.com/carpiediem/memrise-enhancement-suite/wiki/Unofficial-Documentation-for-the-Memrise-API
+
+
+
+Kurs
+
+http://www.memrise.com/api/course/get/?course_id=78623
+
+Level
+
+http://www.memrise.com/api/level/get/?level_id=614731
+ */
+
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
